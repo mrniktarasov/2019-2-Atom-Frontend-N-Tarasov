c5c2f75955290dc75aeb2549e6312cec58d7e8d7
Nikita Tarasov <48064171+mrniktarasov@users.noreply.github.com>
2019-09-29 15:05:21 +0300

    Update convertBytesToHuman.test.js

--- convertBytesToHuman.test.js
+++ convertBytesToHuman.test.js
@@ -13,20 +13,20 @@ test('Возвращает false для неправильного типа да
   expect(convertBytesToHuman("string")).toBe(false)
   expect(convertBytesToHuman(true)).toBe(false)
   expect(convertBytesToHuman([1, 2, 3])).toBe(false)
-  expect(convertBytesToHuman(null).toBe(false))
-  expect(convertBytesToHuman(-1).toBe(false))
-  expect(convertBytesToHuman(Object).toBe(false))
-  expect(convertBytesToHuman(undefined).toBe(false))
-  expect(convertBytesToHuman(Symbol).toBe(false))
+  expect(convertBytesToHuman(null)).toBe(false)
+  expect(convertBytesToHuman(-1)).toBe(false)
+  expect(convertBytesToHuman(Object)).toBe(false)
+  expect(convertBytesToHuman(undefined)).toBe(false)
+  expect(convertBytesToHuman(Symbol)).toBe(false)
 });
 
 test('Возвращает корректное значение для чисел', () => {
-  expect(convertBytesToHuman(256)).toBe("1 B")
-  expect(convertBytesToHuman(1024)).toBe("1 KB")
-  expect(convertBytesToHuman(2e20)).toBe("1 МБ")
-  expect(convertBytesToHuman(2e30)).toBe("1 GB")
-  expect(convertBytesToHuman(2e40)).toBe("1 TB")
-  expect(convertBytesToHuman(2e50)).toBe("1 PB")
+  expect(convertBytesToHuman(256)).toBe("256.00 B")
+  expect(convertBytesToHuman(1024)).toBe("1.00 KB")
+  expect(convertBytesToHuman(2e20)).toBe("1.00 MB")
+  expect(convertBytesToHuman(2e30)).toBe("1.00 GB")
+  expect(convertBytesToHuman(2e40)).toBe("1.00 TB")
+  expect(convertBytesToHuman(2e50)).toBe("1.00 PB")
 });
 
 // другая группа проверок
