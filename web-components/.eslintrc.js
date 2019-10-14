cff297caa94b79c0941baf7589055199a1ef43da
Nikita Tarasov <nikitatarasov987@gmail.com>
2019-10-14 12:06:53 +0300

    eslint fixed

--- web-components/.eslintrc.js
+++ web-components/.eslintrc.js
@@ -4,9 +4,9 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: {
-    'airbnb',
-  },
+  extends: [
+    'airbnb-base',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
