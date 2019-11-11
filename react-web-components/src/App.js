b679cff1846068f9b58e2d275cd121d12aedf9ac
Nikita Tarasov <nikitatarasov987@gmail.com>
2019-11-11 11:24:29 +0300

    Приложение переписано на реакт

---
+++ react-web-components/src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {Groups} from './components/Groups/Groups'
+import {Chat} from './components/Chat/Chat'
+import {AppContext} from './AppContext';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    const info = this.getInfo();
+    this.state = {
+      addNewGroup: false,
+      addNewMessage: false,
+      visibility: 'group',
+      IDgroups : info.IDgroups,
+      groupList: info.groupList,
+    };
+
+    this.newGroup = this.newGroup.bind(this);
+    this.openChat = this.openChat.bind(this);
+    this.closeChat = this.closeChat.bind(this);
+  }
+
+  getInfo () {
+    let info = null;
+    const IDgroups = 'IDgroups';
+    try {
+      info = {
+        groupList: JSON.parse(localStorage.getItem(IDgroups)),
+        IDgroups,
+      }
+    } catch {
+      info = {
+        groupList: null,
+        IDgroups,
+      }
+    }
+    return info;
+  }
+
+  openChat(key) {
+    this.setState({
+      openedChat: key,
+      visibility: 'chat',
+    })
+  }
+
+  closeChat(){
+    this.setState({
+      openedChat: null,
+      visibility: 'group',
+    });
+  }
+
+  newGroup() {
+    this.setState({
+      addNewGroup: true,
+    });
+  }
+
+  newMessage() {
+    this.setState({
+      addNewMessage:true,
+    });
+  }
+
+  render () {
+    return (
+      <AppContext.Provider value={this}>
+        <Groups/>
+        <Chat/>
+      </AppContext.Provider>
+    );
+    }
+}
+
+export default App;
---
+++ react-web-components/src/components/Groups/GroupsPreview/GroupsPreview.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './GroupsPreview.module.css'
+import { AppContext } from '../../../AppContext';
+
+export function GroupsPreview (props){
+    const data = props;
+    return (
+        <AppContext.Consumer>
+        {
+            (value) => (
+                <div className={styles.preview} onClick={value.openChat.bind(value, data.keyGroup)}>
+                    <div className={styles.leftSideWrap}>
+                        <div className={styles.avatar}></div>
+                        <div className={styles.senderInfo}>
+                            <div className={styles.sender}>{data.sender}</div>
+                            <div className={styles.lastMessage}>{data.lastMessage}</div>
+                        </div>
+                    </div>
+                    <div className={styles.metaInfo}>
+                        <div className={styles.time}>{data.lastMessageTime}</div>
+                        <div className={styles.readed}></div>
+                    </div>
+                </div>
+            )
+        }
+        </AppContext.Consumer>
+    );
+}
