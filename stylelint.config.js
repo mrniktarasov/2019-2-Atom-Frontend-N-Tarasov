b6335fb3c7c04eeef90410b51120c7eddd6e5957
Nikita Tarasov <nikitatarasov987@gmail.com>
2019-11-12 23:06:21 +0300

    stylelint errors fixed

--- stylelint.config.js
+++ stylelint.config.js
@@ -9,6 +9,7 @@ module.exports = {
 	],
 	plugins: ['stylelint-no-unsupported-browser-features', 'stylelint-a11y'],
 
+	/*
 	rules: {
 		// ignore special `var-` css variables for `:export`
 		'property-no-unknown': [
@@ -17,9 +18,6 @@ module.exports = {
 				ignoreProperties: ['/^var-/'],
 			},
 		],
-
-		'scale-unlimited/declaration-strict-value': 'off',
-
 		// custom plugins to work with
 		'plugin/no-unsupported-browser-features': [
 			true,
@@ -28,8 +26,35 @@ module.exports = {
 				ignore: ['flexbox'],
 			},
 		],
-
 		// a11y
 		'a11y/content-property-no-static-value': true,
+	},
+	*/
+	rules: {
+		'scale-unlimited/declaration-strict-value': null,
+		'length-zero-no-unit': null,
+		'declaration-block-trailing-semicolon': null,
+		'selector-list-comma-newline-after': null,
+		'csstools/use-nesting': null,
+		'a11y/selector-pseudo-class-focus': null,
+		'color-format/format': null,
+		'color-hex-case': null,
+		'color-named': null,
+		'max-line-length': null,
+		'font-family-no-missing-generic-family-keyword': null,
+		'number-no-trailing-zeros': null,
+		'a11y/media-prefers-reduced-motion': null,
+		'plugin/no-low-performance-animation-properties': null,
+		'selector-pseudo-element-colon-notation': null,
+		'number-leading-zero': null,
+		'block-closing-brace-newline-after': null,
+		'no-eol-whitespace': null,
+		'no-duplicate-selectors': null,
+		'block-opening-brace-space-before': null,
+		indentation: null,
+		'declaration-colon-newline-after': null,
+		'value-list-comma-newline-after': null,
+		'plugin/stylelint-no-indistinguishable-colors': null,
+		'font-weight-notation': null,
 	},
 };
