9180400eb24a7d447195c30f924809f9a141e4be
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-20 13:58:19 +0000

    Deploy mrniktarasov/2019-2-Atom-Frontend-N-Tarasov to github.com/mrniktarasov/2019-2-Atom-Frontend-N-Tarasov.git:gh-pages

---
+++ precache-manifest.0042643e9c569fd9d49467545a0fa642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec3f62245c6ebcdd77166a2bd6ba32f",
+    "url": "/2019-2-Atom-Frontend-N-Tarasov/index.html"
+  },
+  {
+    "revision": "ee27ed4538d9370065e6",
+    "url": "/2019-2-Atom-Frontend-N-Tarasov/static/css/main.61743c92.chunk.css"
+  },
+  {
+    "revision": "76a657c56dd9ff3cabb1",
+    "url": "/2019-2-Atom-Frontend-N-Tarasov/static/js/2.ab09cd30.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/2019-2-Atom-Frontend-N-Tarasov/static/js/2.ab09cd30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee27ed4538d9370065e6",
+    "url": "/2019-2-Atom-Frontend-N-Tarasov/static/js/main.55c5ba61.chunk.js"
+  },
+  {
+    "revision": "42231cea4421d5438b9f",
+    "url": "/2019-2-Atom-Frontend-N-Tarasov/static/js/runtime-main.58b3d6c1.js"
+  }
+]);
